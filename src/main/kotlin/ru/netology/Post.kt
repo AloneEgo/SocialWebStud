ae660239f0fed834caed02c7a182a9ce69642f00
Oleg Yakovenko <sc0rpi0n2001@yandex.com>
2025-05-12 22:16:41 +0300

    Nullable values added

--- src/main/kotlin/ru/netology/Post.kt
+++ src/main/kotlin/ru/netology/Post.kt
@@ -4,13 +4,13 @@ data class Post(
     val replyPostId: Int,
     val ownerId: Int,
     val fromId: Int,
-    val text: String,
+    val text: String?,
     val comments: Comments,
-    val likes: Likes,
+    val likes: Likes?,
     val postType: String = "post",
     val friendOnly: Boolean = false,
     val isFavorite: Boolean = false,
     val isPinned: Boolean = false,
-    val id: Int = 0,
-    val date: Int = 0
+    val id: Int? = 0,
+    val date: Int? = 0
 )
